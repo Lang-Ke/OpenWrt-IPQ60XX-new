990ca2d9f73a68477eae402a77a8baab3f3bd5c2
Lang-Ke <80813300+Lang-Ke@users.noreply.github.com>
2026-01-04 18:45:55 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -1,3 +1,5 @@
+rm -rf feeds/luci/applications/luci-app-attendedsysupgrade
+rm -rf feeds/packages/utils/attendedsysupgrade
 rm -rf package/emortal/luci-app-athena-led
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
